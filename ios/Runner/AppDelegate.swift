96a3ce616e3fe883434ded562885aa58649c88c1
canxin <q1969730106@gmail.com>
2024-03-27 19:14:08 +0800

    1

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import flutter_downloader
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,28 +8,6 @@ import flutter_downloader
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    FlutterDownloaderPlugin.setPluginRegistrantCallback(registerPlugins)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  // https://stackoverflow.com/a/66997677
-  override func applicationWillResignActive(
-    _ application: UIApplication
-  ) {
-    let secureKey = "flutter.useSecureMode"
-    if UserDefaults.standard.bool(forKey: secureKey) {
-      self.window.isHidden = true;
-    }
-  }
-  override func applicationDidBecomeActive(
-    _ application: UIApplication
-  ) {
-    self.window.isHidden = false;
-  }
-}
-
-private func registerPlugins(registry: FlutterPluginRegistry) {
-  if (!registry.hasPlugin("FlutterDownloaderPlugin")) {
-    FlutterDownloaderPlugin.register(with: registry.registrar(forPlugin: "FlutterDownloaderPlugin")!)
   }
 }
