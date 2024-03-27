cdddb5c81c3b206dc456bee1539c7cf89c1fb824
canxin <q1969730106@gmail.com>
2024-03-27 18:57:31 +0800

    init mine

--- rust/src/api/simple.rs
+++ rust/src/api/simple.rs
@@ -1,7 +1,6 @@
 // This source code is a part of Project Violet.
 // Copyright (C) 2020-2024. violet-team. Licensed under the Apache-2.0 License.
 
-use crate::utils::decompress::to_parent_entry_extract_fn;
 
 #[flutter_rust_bridge::frb(init)]
 pub fn init_app() {
@@ -12,9 +11,4 @@ pub fn init_app() {
 #[flutter_rust_bridge::frb(sync)] // Synchronous mode for simplicity of the demo
 pub fn greet(name: String) -> String {
     format!("Hello, {name}!")
-}
-
-pub async fn decompress_7z(src: String, dest: String) {
-    sevenz_rust::decompress_file_with_extract_fn(src, dest, to_parent_entry_extract_fn)
-        .expect("complete");
 }
